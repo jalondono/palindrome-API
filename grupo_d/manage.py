c28752738ad6663d5ed38608b853af659822c68d
jalondono <juanlondono151776@hotmail.com>
2021-02-14 13:27:39 -0500

    [update] Added dockerfile and docker compose * created settings file dev in sqlite

--- grupo_d/manage.py
+++ grupo_d/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grupo_d.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grupo_d.settings.dev_sqlite')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
